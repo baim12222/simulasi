f162346fd61fec7018e4a193281b8fbff7fe2508
baim12222 <aleembaim@gmail.com>
2024-11-10 21:40:53 +0700

    Update aleeeee.py

--- aleeeee.py
+++ aleeeee.py
@@ -1,57 +1,54 @@
 import tkinter as tk
-import tkinter.messagebox
+import tkinter.messagebox 
 from tkinter.constants import SUNKEN
 
 window = tk.Tk()
-window.title('Aleem Ahmad')
+window.title('ALEEM AHMAD')
+
 window.attributes('-fullscreen', True)
+
 frame = tk.Frame(master=window, bg="grey",padx=10)
 frame.pack(fill=tk.BOTH, expand=True)
-entry = tk.Entry(master=frame, relief=SUNKEN,borderwidth=3, width=30)
-entry.grid(row=0, column=0, columnspan=3, ipady=2, pady=2, sticky="nsew")
+entry = tk.Entry(master=frame, relief=SUNKEN,
+ borderwidth=3, width=30)
+entry.grid(row=0, column=0, columnspan=4, ipady=2, pady=2, sticky="nsew")
 
 def myclick(number):
-    entry.insert(tk.END, number)
+    entry.insert(tk.END,number)
 
 def equal():
     try:
-        result = eval(entry.get().replace(',', '.'))
+        y = str(eval(entry.get()))
         entry.delete(0, tk.END)
-        entry.insert(0, str(result))
-    except Exception:
-        entry.delete(0, tk.END)
-        entry.insert(0, "Error")
+        entry.insert(0,y)
+    except:
+        tkinter.messagebox.showinfo("Bermasalah","syntax Error")
 
 def comma():
     current = entry.get()
-    if ',' not in current:
+    if current == "":
+        entry.insert(tk.END, '0,')
+    elif current[-1] != ',':
         entry.insert(tk.END, ',')
-        
-def percentage() :
+
+def percentage():
     current = entry.get()
     try:
-        if not current:
-            return
         result = eval(current.replace(',', '.')) / 100
-        entry.delete(0,tk.END)
+        entry.delete(0, tk.END)
         entry.insert(0, str(result))
     except Exception as e:
         entry.delete(0, tk.END)
-        entry.insert(0, "Error")
+        entry.insert(0, "Bermasalah")
 
-def power():
-    current = entry.get()
+def equal():
     try:
-        base, exponent = map(float, current.split('^'))  # expects input like "2^3"
-        result = base ** exponent
+        result = eval(entry.get().replace(',', '.').replace('^',''))
         entry.delete(0, tk.END)
         entry.insert(0, str(result))
-    except ValueError:
+    except Exception as e:
         entry.delete(0, tk.END)
-        entry.insert(0, "Error")
-    except Exception:
-        entry.delete(0, tk.END)
-        entry.insert(0, "Error")
+        entry.insert(0, "Bermasalah")
 
 def square_root():
     current = entry.get()
@@ -61,61 +58,61 @@ def square_root():
         entry.insert(0, str(result))
     except ValueError:
         entry.delete(0, tk.END)
-        entry.insert(0, "Error")
+        entry.insert(0, "Bermasalah")
 
 def clear():
     entry.delete(0, tk.END)
 # Pembuatan tombol angka dan operasi
 
 button_1 = tk.Button(master=frame, text='1', padx=15,
-            pady=5, width=3, bg='grey', fg='white', command=lambda:
+            pady=5, width=3, bg='grey', fg='#004d4d', command=lambda:
 myclick(1))
 button_1.grid(row=2, column=0, pady=2, sticky="nsew")
 button_2 = tk.Button(master=frame,text='2', padx=15,
-            pady=5, width=3, bg='grey', fg='white', command=lambda: 
+            pady=5, width=3, bg='grey', fg='#004d4d', command=lambda: 
 myclick(2))       
 button_2.grid(row=2, column=1, pady=2, sticky="nsew")
 button_3 = tk.Button(master=frame,text='3', padx=15, 
-            pady=5, width=3, bg='grey', fg='white', command=lambda: 
+            pady=5, width=3, bg='grey', fg='#004d4d', command=lambda: 
 myclick(3))       
 button_3.grid(row=2, column=2, pady=2, sticky="nsew")
 button_4 = tk.Button(master=frame,text='4', padx=15, 
-            pady=5, width=3, bg='grey', fg='white', command=lambda: 
+            pady=5, width=3, bg='grey', fg='#004d4d', command=lambda: 
 myclick(4))       
 button_4.grid(row=3, column=0, pady=2, sticky="nsew")
 button_5 = tk.Button(master=frame,text='5', padx=15, 
-            pady=5, width=3, bg='grey', fg='white', command=lambda: 
+            pady=5, width=3, bg='grey', fg='#004d4d', command=lambda: 
 myclick(5))       
 button_5.grid(row=3, column=1, pady=2, sticky="nsew")
 button_6 = tk.Button(master=frame,text='6', padx=15, 
-            pady=5, width=3, bg='grey', fg='white', command=lambda: 
+            pady=5, width=3, bg='grey', fg='#004d4d', command=lambda: 
 myclick(6))       
 button_6.grid(row=3, column=2, pady=2, sticky="nsew")
 button_7 = tk.Button(master=frame,text='7', padx=15, 
-            pady=5, width=3, bg='grey', fg='white', command=lambda: 
+            pady=5, width=3, bg='grey', fg='#004d4d', command=lambda: 
 myclick(7))       
 button_7.grid(row=4, column=0, pady=2, sticky="nsew")
 button_8 = tk.Button(master=frame,text='8', padx=15, 
-            pady=5, width=3, bg='grey', fg='white', command=lambda: 
+            pady=5, width=3, bg='grey', fg='#004d4d', command=lambda: 
 myclick(8))       
 button_8.grid(row=4, column=1, pady=2, sticky="nsew")
 button_9 = tk.Button(master=frame,text='9', padx=15, 
-            pady=5, width=3, bg='grey', fg='white', command=lambda: 
+            pady=5, width=3, bg='grey', fg='#004d4d', command=lambda: 
 myclick(9))       
 button_9.grid(row=4, column=2, pady=2, sticky="nsew")
 
 button_0 = tk.Button(master=frame,text='0', padx=15, 
-            pady=5, width=3, bg='grey', fg='white', command=lambda: 
+            pady=5, width=3, bg='grey', fg='#004d4d', command=lambda: 
 myclick(0))       
 button_0.grid(row=5, column=1, pady=2, sticky="nsew")
 
 button_00 = tk.Button(master=frame,text='00', padx=15, 
-            pady=5, width=3, bg='grey', fg='white', command=lambda: 
+            pady=5, width=3, bg='grey', fg='#004d4d', command=lambda: 
 myclick('00'))       
 button_00.grid(row=5, column=0, pady=2, sticky="nsew")
 
 button_000 = tk.Button(master=frame,text='000', padx=15, 
-            pady=5, width=3, bg='grey', fg='white', command=lambda: 
+            pady=5, width=3, bg='grey', fg='#004d4d', command=lambda: 
 myclick('000'))       
 button_000.grid(row=5, column=2, pady=2, sticky="nsew")
 
@@ -139,10 +136,10 @@ button_div = tk.Button(master=frame, text="/", padx=15,
 myclick('/'))
 button_div.grid(row=1, column=3, pady=2, sticky="nsew")
 
-button_power = tk.Button(master=frame, text="^", padx=15,
-                        pady=5, width=3, bg='#004d4d', fg='white', command=power)
-button_power.grid(row=5, column=3, pady=2, sticky="nsew")
-
+button_pangkat = tk.Button(master=frame, text="^", padx=15,
+                        pady=5, width=3, bg='#004d4d', fg='white', command=lambda:
+myclick('^'))
+button_pangkat.grid(row=5, column=3, pady=2, sticky="nsew")
 
 button_square = tk.Button(master=frame, text="√", padx=15,
                         pady=5, width=3, bg='#004d4d', fg='white', command=square_root)
@@ -159,7 +156,7 @@ button_percentage = tk.Button(master=frame, text="%", padx=15,
 button_percentage.grid(row=2, column=3, pady=2, sticky="nsew")
 
 button_clear = tk.Button(master=frame, text="clear", padx=15,
-                        pady=5,width=12, bg='#004d4d', fg='white', 
+                        pady=5,width=12, bg='darkblue', fg='white', 
 command=clear)
 button_clear.grid(row=0, column=3, pady=2, sticky="nsew")
 
